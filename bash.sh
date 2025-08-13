ee80965a1517c11e0e2fc0cdd4ac3f952eefbb7b
skyportlab <elproxdqqppq@gmail.com>
2025-08-13 19:45:36 -0400

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script para instalar Remote.it con registro
+
+R3_REGISTRATION_CODE="886DABAA-DD4C-5F4F-8375-F39F0B8EB639"
+sh -c "$(curl -L https://downloads.remote.it/remoteit/install_agent.sh)"
